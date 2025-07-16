c080124cddd94e4a92698a976c2e037e9829842c
git <pavol1gajdos@gmail.com>
2025-07-16 15:57:02 +0200

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #run on waitress-serve production server
-gunicorn -b 0.0.0.0:5000 -k gevent -w 20 --threads 20 -t 0 --reload web:app
+gunicorn web:app -b 0.0.0.0:5000 -k gevent -w 20 --threads 20 -t 0 --reload --reload-extra-file lasilla_config.txt $(find templates -type f -name '*' -exec echo --reload-extra-file {} \;)
